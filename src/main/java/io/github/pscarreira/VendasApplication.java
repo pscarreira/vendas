5903bdd413a507001fdb7580ca7c6d1e4d0c0145
Paulo Carreira <psalvescarreira@gmail.com>
2022-02-13 17:14:23 -0300

    Configurando banco de dados H2

--- src/main/java/io/github/pscarreira/VendasApplication.java
+++ src/main/java/io/github/pscarreira/VendasApplication.java
@@ -1,9 +1,14 @@
 package io.github.pscarreira;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
   public static void main(String[] args) {
     SpringApplication.run(VendasApplication.class, args);
